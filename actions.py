75c1c9ddbd6f6036567121fbb3471d1e996898c4
Henry Desktop <henrypmoore123@gmail.com>
2020-11-04 14:52:54 -0500

    just added ItemAction to actions.py

--- actions.py
+++ actions.py
@@ -7,7 +7,7 @@ import exceptions
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Actor, Entity
+    from entity import Actor, Entity, Item
 
 
 class Action:
@@ -28,6 +28,26 @@ class Action:
         """
         raise NotImplementedError()
 
+class ItemAction(Action):
+    def __init__(
+        self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+    ):
+        super().__init__(entity)
+        self.item = item
+
+        if not target_xy:
+            target_xy = entity.x, entity.y
+
+        self.target_xy = target_xy
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        # Returns the actor at this actions destination
+        return self.engine.game_map.get_actor_at_location(*self.target_xy)
+    
+    def perform(self) -> None:
+        # Invoke the items ability, this action provides context
+        self.item.consumable.activate(self)
 
 class EscapeAction(Action):
     def perform(self) -> None:
